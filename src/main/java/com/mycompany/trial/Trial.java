a1302ea29288c99da4ebb61a54af0d72844d34d3
jpierog9 <jacobpierog@gmail.com>
2023-02-06 23:28:15 -0500

    1st

---
+++ src/main/java/com/mycompany/trial/Trial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trial;
+
+/**
+ *
+ * @author jacob
+ */
+public class Trial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
