7fba76ec67d409b9a213185aecc39394aff1d054
jpierog9 <jacobpierog@gmail.com>
2023-02-06 23:36:28 -0500

    2nd

--- src/main/java/com/mycompany/trial/Trial.java
+++ src/main/java/com/mycompany/trial/Trial.java
@@ -12,5 +12,8 @@ public class Trial {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("Cock");
+        //cock
     }
 }
